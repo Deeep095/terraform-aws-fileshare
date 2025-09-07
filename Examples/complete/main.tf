f2f4425928d569d362f804c22a996cb668563996
Deeep_095 <deeepprojects@gmail.com>
2025-09-07 18:49:43 +0530

    change the name of repo

--- Examples/complete/main.tf
+++ Examples/complete/main.tf
@@ -1,19 +1,7 @@
-terraform{
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 6.0"
-      }
-    }
-}
-
-provider "aws" {
-  region = "preferred-aws-region" 
-}
 
 module "fileshare" {
   source = "./modules"
 
   notify_email = "your-email-id"
-  
+  aws_region   = "preferred-aws-region"
 }
